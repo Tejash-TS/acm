5be53e6c0a9a48258180b863dbb6602b65bc85d3
Tejash-TS <tejashsananse@gmail.com>
2025-04-13 19:44:55 +0530

    added cods

---
+++ assets/js/scroll.js
@@ -0,0 +1,16 @@
+ScrollReveal
+	({
+		reset:true,
+		distance:'60px',
+		duration: 2500,
+		delay: 400
+	});
+	
+	ScrollReveal().reveal('.scroll_left',{delay:600, origin:'left'});
+	ScrollReveal().reveal('.scroll_right',{delay:700, origin:'right'});
+	ScrollReveal().reveal('.scroll_top',{delay:500, origin:'top'});
+	ScrollReveal().reveal('.scroll_bottom',{delay:750, origin:'bottom'});
+	ScrollReveal().reveal('.scroll_bottom1',{delay:400, origin:'bottom'});
+	ScrollReveal().reveal('.scroll_bottom_a',{delay:780, origin:'bottom'});
+	ScrollReveal().reveal('.scroll_bottom_b',{delay:810, origin:'bottom'});
+	ScrollReveal().reveal('.scroll_bottom_c',{delay:840, origin:'bottom'});
